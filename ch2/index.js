fade53cf862e8c85d2558192295ed27a9d255cf6
Mohit-Srivastava-Official <mohitsrivastavaofficial@gmail.com>
2023-12-13 23:43:01 +0530

    ch2

--- ch2/index.js
+++ ch2/index.js
@@ -1,1 +1,12 @@
-s
+//1.Check wheteher the age is b/w 10 and 20 using logical operator in Java_Script
+let age=19;
+console.log(age);
+if(age>10 && age<20)
+{
+    console.log("Your age is b/w 10 and 20");
+}
+else
+{
+    console.log("Your age is not b/w 10 and 20");
+
+}
