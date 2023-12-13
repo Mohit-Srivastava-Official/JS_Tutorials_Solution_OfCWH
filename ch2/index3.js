058ece5d300077db75574a9327749785a1f26942
Mohit-Srivastava-Official <mohitsrivastavaofficial@gmail.com>
2023-12-13 23:54:16 +0530

    ch2

---
+++ ch2/index3.js
@@ -0,0 +1,12 @@
+//3.To check whether a number is divisible by 2 and 3
+//4.Do it yourself
+
+let a=8;
+if(a%2==0 && a%3==0)
+{
+    console.log("Number is divisible by 2 and 3");
+}
+else
+{
+    console.log("Number is not divisible by 2 and 3");
+}
