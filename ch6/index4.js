467e77e59fbf27114e96ea886c5b5fc4ff6ade82
Mohit-Srivastava-Official <mohitsrivastavaofficial@gmail.com>
2023-12-19 00:38:54 +0530

    ch6

--- ch6/index4.js
+++ ch6/index4.js
@@ -1,17 +1,21 @@
 let x=prompt("Choose B:(Blue) or R:(Red) or Y:(Yellow)")
-if(x==B || x==b || x==Blue)
+if(x=='B' || x=='b' || x=='Blue')
 {
     document.body.style.backgroundColor="blue";
 
 }
-elseif(x==R || x==r || x==Red)
+else if(x=='R' || x=='r' || x=="Red")
 {
 
     document.body.style.backgroundColor="red";
 
 }
-elseif(x==Y || x==y || x==Yellow)
+else if(x=='Y' || x=='y' || x=='Yellow')
 {
     document.body.style.backgroundColor="yellow";
 
+}
+else
+{
+    alert("Invalid Code")
 }
