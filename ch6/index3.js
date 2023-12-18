750d7e0302e481fbce14f46e9df73f2bc4c27c4b
Mohit-Srivastava-Official <mohitsrivastavaofficial@gmail.com>
2023-12-19 00:23:46 +0530

    ch6

--- ch6/index3.js
+++ ch6/index3.js
@@ -1,1 +1,6 @@
-//Writing a program to change the url to google.com (redirection) if user entera a number greater than 4
+//4.Writing a program to change the url to google.com (redirection) if user enter a number greater than 4
+let age=prompt("Enter a number");
+if(age>4)
+{
+    location.href="https://www.google.com"
+}
---
+++ ch6/index4.js
@@ -0,0 +1,17 @@
+let x=prompt("Choose B:(Blue) or R:(Red) or Y:(Yellow)")
+if(x==B || x==b || x==Blue)
+{
+    document.body.style.backgroundColor="blue";
+
+}
+elseif(x==R || x==r || x==Red)
+{
+
+    document.body.style.backgroundColor="red";
+
+}
+elseif(x==Y || x==y || x==Yellow)
+{
+    document.body.style.backgroundColor="yellow";
+
+}
