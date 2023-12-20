5381ff02dd86c4d0300c12331c45d3b4766f37ac
Mohit-Srivastava-Official <mohitsrivastavaofficial@gmail.com>
2023-12-20 17:27:08 +0530

    ch7

---
+++ ch7/index2.js
@@ -0,0 +1,4 @@
+//2.Creating a table without tbody.Now,using view page source button to check whether it has tbody or not.
+//Firstly let us take an example of using table.
+//Maybe no.
+console.log(table.tBodies);
---
+++ ch7/index3.js
@@ -0,0 +1,4 @@
+// let boxes=document.getElementsByClassName("box");
+// // boxes[0].style.color="green";
+// boxes[2].style.color="green";
+document.firstElementChild.style.color="yellow";
