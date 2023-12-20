574bcd5b59dc47cb1ee91015e84e024016805c2a
Mohit-Srivastava-Official <mohitsrivastavaofficial@gmail.com>
2023-12-20 16:37:15 +0530

    ch7

---
+++ ch7/index.js
@@ -0,0 +1,5 @@
+//1. Creating a navbar and changing the color of its first element.
+let boxes=document.getElementsByClassName("box");
+boxes[0].style.backgroundColor="red";
+// let boxes=document.getElementsByClassName("box");
+// boxes[0].style.backgroundColor="red";
