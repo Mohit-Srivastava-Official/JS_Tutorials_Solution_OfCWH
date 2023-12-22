76cca4604b1d3838bbbf31cbcd43580a0fa49193
Mohit-Srivastava-Official <mohitsrivastavaofficial@gmail.com>
2023-12-22 12:07:58 +0530

    ch7

--- ch7/index5.js
+++ ch7/index5.js
@@ -1,1 +1,8 @@
 //4.JavaScript code to change background of all <li> tags to cyan
+var x= document.querySelectorAll("li");
+for(let i=0;i<x.length;i++)
+{
+    x[i].style.backgroundColor="cyan";
+}
+//5.Which of the following is used to look for the farthest ancestor that matched a given css selector
+//Ans:The :host-context() pseudo-class selector is used to look for the farthest ancestor that matched a given CSS selector
