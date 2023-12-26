8510dff45a1a1ca8d70f823d774973a5e30c7306
Mohit-Srivastava-Official <mohitsrivastavaofficial@gmail.com>
2023-12-26 19:48:25 +0530

    ch8

---
+++ ch8/script11.js
@@ -0,0 +1,5 @@
+document.querySelector(".top").addEventListener("click",(e) => {
+   <p><a href="#last">sasdasda</a></p>
+// alert("Child was Clicked")
+}
+)
---
+++ ch8/script9.js
@@ -0,0 +1,4 @@
+function funct()
+{
+    alert("Alert_2 was triggered");
+}
